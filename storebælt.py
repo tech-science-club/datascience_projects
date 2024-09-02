1189681088663dc7c6a9565c056a0009eb68fff7
techmadman <dimapanas87@gmail.com>
2024-09-02 11:12:19 +0200

    add files

---
+++ storebælt.py
@@ -0,0 +1,196 @@
+import io
+import os
+import pandas as pd
+import matplotlib as mpl
+import xlsxwriter
+from xlsxwriter import Workbook
+import seaborn as sns
+import matplotlib.pyplot as plt
+import openpyxl
+# import BarChart class from openpyxl.chart sub_module
+from openpyxl.chart import BarChart,Reference
+
+data = pd.read_csv('trafficdata.csv', sep=";", encoding='ISO-8859-1')
+
+count = 0
+start_year = 2024
+MC_og_Bil  = 0
+Bil_3_6_m = 0
+Bil_anhænger = 0
+Små_lastbiler = 0
+Store_lastbiler = 0
+Lastbiler_over_20_meter = 0
+Busser = 0
+frames = {}
+
+#prices:
+MC_og_Bil_price  = 100
+Bil_3_6_m_price = 200
+Bil_anhænger_price = 300
+Små_lastbiler_price = 300
+Store_lastbiler_price =936
+Lastbiler_over_20_meter_price = 1401
+Busser_price = 936
+
+for index, row in data.iterrows():
+
+    filtered_data = data[data['År'] == start_year]
+    frames[f'frame_{start_year}'] = filtered_data
+    MC_og_Bil= filtered_data['MC og Bil'].sum()
+    Bil_3_6_m = filtered_data['Bil 3-6m'].sum()
+    Bil_anhænger = filtered_data['Bil+anhænger'].sum()
+    Små_lastbiler =filtered_data['Små lastbiler'].sum()
+    Store_lastbiler = filtered_data['Store lastbiler'].sum()
+    Lastbiler_over_20_meter = filtered_data['Lastbiler over 20 meter'].sum()
+    Busser = filtered_data['Busser'].sum()
+
+    items = [
+        'MC og Bil', 'Bil 3-6m', 'Bil+anhænger', 'Små lastbiler',
+        'Store lastbiler', 'Lastbiler over 20 meter', 'Busser'
+    ]
+    Sum = [
+        MC_og_Bil, Bil_3_6_m, Bil_anhænger, Små_lastbiler,
+        Store_lastbiler, Lastbiler_over_20_meter, Busser
+    ]
+    #-----------------plot for each year---------------------------------------
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=items, y=Sum, width=0.5, hue=items )
+    plt.xticks(rotation=20, ha='right')
+    plt.title(f'Traffic in {start_year}')
+    plt.xlabel('Transport type')
+    plt.ylabel('Amount vehicles in mln')
+    plt.tight_layout()
+    #plt.show()
+    plt.savefig(f'{start_year}.png')
+    plt.close()
+
+    with pd.ExcelWriter(f'{start_year}.xlsx', engine='xlsxwriter') as writer:
+        filtered_data.to_excel(writer, sheet_name='Data', index=False)
+        workbook = writer.book
+        worksheet = writer.sheets['Data']
+        imgdata = io.BytesIO()
+        worksheet.insert_image(20, 0, f'{start_year}.png')
+    os.remove(f'{start_year}.png')
+
+    print(frames[f'frame_{start_year}'])
+    print("MC+small car: " + str(MC_og_Bil))
+    print("car 3-6m: "+str(Bil_3_6_m))
+    print("Car+trailer: "+str(Bil_anhænger))
+    print("Small tracks: " +str(Små_lastbiler))
+    print("Big tracks: " + str(Store_lastbiler))
+    print("Tracks 20m +: " + str(Lastbiler_over_20_meter))
+    print("Buses: "+  str(Busser))
+
+    start_year -= 1
+    if start_year < 1998:
+        break
+
+
+val1= data['MC og Bil'].sum()
+val2 = data['Bil 3-6m'].sum()
+val3 = data['Bil+anhænger'].sum()
+val4 = data['Små lastbiler'].sum()
+val5 = data['Store lastbiler'].sum()
+val6 = data['Lastbiler over 20 meter'].sum()
+val7 = data['Busser'].sum()
+
+
+val1_price = MC_og_Bil_price*val1
+val2_price = Bil_3_6_m_price * val2
+val3_price =  Bil_anhænger_price *val3
+val4_price =  Små_lastbiler_price *val4
+val5_price =  Store_lastbiler_price *val5
+val6_price =  Lastbiler_over_20_meter_price *val6
+val7_price =  Busser_price *val7
+total_revenue = val1_price + val2_price + val3_price + val4_price + val5_price + val6_price + val7_price
+
+total_items = [
+                'MC+small car',
+                'car_3_6m',
+                'Car_trailer',
+                'Small_tracks',
+                'Big_tracks',
+                'Tracks_20m',
+                'Buses'
+              ]
+
+total_amaunt = [
+                val1,
+                val2,
+                val3,
+                val4,
+                val5,
+                val6,
+                val7,
+                ]
+total_rev = [
+                val1_price,
+                val2_price,
+                val3_price,
+                val4_price,
+                val5_price,
+                val6_price,
+                val7_price,
+            ]
+
+result_df = pd.DataFrame(
+    {
+        'items':  total_items,
+        'amount of vehicles': total_amaunt,
+        'total profit': total_rev,
+        'currency': "dkk"
+    }
+)
+print("------------------------total----------------------------")
+print(result_df)
+print(f"total_revenue {total_revenue} dkk")
+
+
+plt.figure(figsize=(8, 6))
+plots = sns.barplot(x=total_items, y=total_amaunt, width=0.75, gap=0.1, hue=items, legend=False)
+for bar in plots.patches:
+    plots.annotate(format(bar.get_height(), '.2e'),
+                   (bar.get_x() + bar.get_width() / 2,
+                    bar.get_height()), ha='center', va='center',
+                   size=10, xytext=(0, 8),
+                   textcoords='offset points')
+
+plt.xticks(rotation=20, ha='right')  # Rotate x labels for better readability
+plt.title('Storbælt traffic 1998-2024 years', ha='center')  
+plt.xlabel('Transport type')
+plt.ylabel('Amount vehicles in mln')
+plt.tight_layout()  # Adjust layout to fit labels
+plt.savefig('barplot.png')
+
+
+#----------------------------------------------------------------------------
+#pie chart view
+
+explode = (0, 0.1, 0, 0, 0, 0.35, 0.25)
+fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(aspect="equal"))
+ax.pie(total_amaunt, autopct='%1.1f%%',  explode = explode, startangle=145, shadow=False, textprops={'size': 'smaller'},  pctdistance = 1.1,  wedgeprops=dict(width=0.5))
+plt.text(x=1.5, y=-1.5, s=f"Total revenue:\n{total_revenue} dkk", fontsize=12, color='Black', ha='center')
+plt.tight_layout()
+ax.legend(items, title="Transport", loc="upper left", bbox_to_anchor=(1, 0, 0.5, 1), fontsize='small', shadow=True)
+fig.suptitle("Storebelt traffic, 1998-2024 years", fontsize=16)
+plt.tight_layout()
+plt.savefig('pie_chart.png')
+plt.show()
+
+with pd.ExcelWriter('storbelt_data.xlsx', engine='xlsxwriter') as writer:
+    # Write the DataFrame to Excel
+    data.to_excel(writer, sheet_name='Data', index=False)
+    result_df.to_excel(writer, sheet_name='Data', index=False, startrow = 0, startcol = 11)
+    workbook = writer.book
+    worksheet = writer.sheets['Data']
+    #worksheet.write_string(0, 317, out_str)
+
+    imgdata = io.BytesIO()
+    worksheet.insert_image(0, 17, 'pie_chart.png')
+    worksheet.insert_image(40, 17, 'barplot.png')
+
+
+
+
+
+
---
+++ weather_history.py
@@ -0,0 +1,173 @@
+import openmeteo_requests
+import matplotlib.pyplot as plt
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+import seaborn as sns
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 55.7281,               # Denmark, Svinninge
+	"longitude": 11.5409,              #
+	#"latitude": 31.392222,            #
+	#"longitude": 48.999722,           # Ukraine, Shpola
+	"start_date": "2024-05-01",
+	"end_date": "2024-08-20",
+	"hourly": ["temperature_2m", "cloud_cover" ],
+	"daily": ["temperature_2m_max", "temperature_2m_min", "precipitation_sum", "rain_sum", "wind_speed_10m_max"],
+	"timezone": "Europe/Berlin"
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_cloud_cover = hourly.Variables(1).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["cloud_cover"] = hourly_cloud_cover
+
+
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+
+# Count the number of occurrences for each unique date
+hourly_dataframe['date_only'] = hourly_dataframe['date'].dt.date
+
+# Group by the extracted date and calculate the mean of each group
+daily_averages = hourly_dataframe.groupby('date_only').mean().reset_index()
+
+# Rename columns for clarity (optional)
+daily_averages = daily_averages.rename(columns={
+	'date_only': 'date',
+	'cloud_cover': 'average_cloud_cover'
+})
+
+#-----------------------------------------------------------------------------------------------------
+# Process daily data. The order of variables needs to be the same as requested.
+daily = response.Daily()
+daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+daily_precipitation_sum = daily.Variables(2).ValuesAsNumpy()
+daily_rain_sum = daily.Variables(3).ValuesAsNumpy()
+daily_wind_speed_10m_max = daily.Variables(4).ValuesAsNumpy()
+
+daily_data = {"date": pd.date_range(
+	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = daily.Interval()),
+	inclusive = "left"
+)}
+daily_data["temperature_2m_max"] = daily_temperature_2m_max
+daily_data["temperature_2m_min"] = daily_temperature_2m_min
+daily_data["precipitation_sum"] = daily_precipitation_sum
+daily_data["rain_sum"] = daily_rain_sum
+daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
+
+daily_dataframe = pd.DataFrame(data = daily_data)
+
+print(daily_dataframe.head(20))
+#--------------> join 2 tables
+daily_dataframe['date'] = daily_dataframe['date'].dt.date
+
+join_tables = pd.merge(daily_dataframe, daily_averages[['date', 'average_cloud_cover']], on='date', how='left')
+
+count_temp = 0
+count_temp_min = 0
+count_wind = 0
+count_rain = 0
+count_summer = 0
+count_clouds = 0
+
+for index, row in join_tables.iterrows():
+
+	temp = row['temperature_2m_max']
+	wind_speed = row['wind_speed_10m_max']
+	rain = row['rain_sum']
+	cloud_cover = row['average_cloud_cover']
+	if cloud_cover < 25:
+		count_clouds += 1
+	if temp > 22.0:
+		count_temp += 1
+	if wind_speed < 25:
+		count_wind += 1
+	if rain < 5.0:
+		count_rain += 1
+	if temp > 22.0 and  wind_speed < 25 and  rain < 5.0 and cloud_cover < 25:
+		count_summer+=1
+
+print(f"Amount of warm days: {count_temp}")
+print(f"with out wind: {count_wind}")
+print(f"with out rain: {count_rain}")
+print(f"with sunny weather, <25% clouds: {count_clouds}")
+print(f"Coincidence of factors above, consider as summer: {count_summer}")
+#--------------------------create plots---------------------------------------------
+
+#matplotlib
+#join_tables.plot('date', 'rain_sum', label='rain_sum', color='red')
+#join_tables.plot('date', 'temperature_2m_max', linewidth=2, label='temp', color='blue')
+#join_tables.plot('date', 'wind_speed_10m_max', linewidth=2, label='wind_speed_10m_max', color='green')
+#join_tables.plot('date','average_cloud_cover', label='overcast stat distirbution % ', color='blue')
+#sns.displot(join_tables, x="average_cloud_cover", binwidth=3, stat="density", color="skyblue", label="Overcast stat distib.")
+#sns.displot(join_tables, x="wind_speed_10m_max", binwidth=2, stat="density", color="cyan", label="wind speed stat dist.")
+
+#seaborn
+fig, axes = plt.subplots(2, 2, figsize=(10, 8))
+
+# First plot: Distribution of Cloud Cover
+sns.histplot(join_tables, x="average_cloud_cover",  binwidth=2, color="blue", ax=axes[0, 0])
+axes[0, 0].set_title("Average Overcast Distribution")
+axes[0, 0].set_xlabel(" overcast (%)")
+axes[0, 0].set_ylabel("Frequency")
+
+# Second plot: Temperature vs. Cloud Cover
+sns.lineplot(join_tables, x="date", y="temperature_2m_max", ax=axes[0, 1], color="red")
+axes[0, 1].set_title("Max Temperature")
+axes[0, 1].set_xlabel("Date")
+axes[0, 1].set_ylabel("Temperature (°C)")
+
+# Third plot: Wind Speed
+sns.histplot(join_tables, x="wind_speed_10m_max",  binwidth=2, color="cyan", ax=axes[1, 0])
+axes[1, 0].set_title("Wind Speed Distribution")
+axes[1, 0].set_xlabel("Wind Speed (km/h)")
+axes[1, 0].set_ylabel("Frequency")
+
+# Fourth plot: rain
+sns.lineplot(join_tables, x="date", y="rain_sum", ax=axes[1, 1], color="purple")
+axes[1, 1].set_title("Precipitation")
+axes[1, 1].set_xlabel("Date")
+axes[1, 1].set_ylabel("Rain (mm)")
+
+for ax in axes.flat:
+	ax.set_xticklabels(ax.get_xticklabels(), rotation=20)# Adjust layout
+
+plt.tight_layout()
+fig.suptitle("Multiple Climate Metrics", fontsize=18)
+plt.subplots_adjust(top=0.93)
+
+# Add a global title for all plots
+fig.suptitle("Climate Data Analysis", fontsize=16)
+plt.subplots_adjust(top=0.9)  # Adjust the top to make space for the global title
+
+plt.show()
+
